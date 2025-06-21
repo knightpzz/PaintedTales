2befaf182e6882e429b7b8bb869eac0818185c68
mitaka <2996012639@qq.com>
2025-06-21 14:57:29 +0800

    修改了history无法显示时间的bug
    哦耶哦耶哦耶哦耶哦耶哦耶哦耶哦耶哦耶哦耶哦耶哦耶哦耶哦耶哦耶哦耶哦耶哦耶哦耶哦耶哦耶哦耶哦耶哦耶哦耶哦耶哦耶哦耶

--- miniprogram/page/history/index.js
+++ miniprogram/page/history/index.js
@@ -13,6 +13,14 @@ Page({
         this.setData({
           historyList: res.data
         })
+        this.data.historyList = this.data.historyList.map(item => {
+          // 确保 createdAt 字段是 Date 对象
+          item.createdAt = new Date(item.createdAt).toLocaleString();
+          return item;
+        });
+        this.setData({
+          historyList: this.data.historyList  // 更新数据
+        })
       })
       .catch(err => {
         console.error('❌ 获取失败', err)
