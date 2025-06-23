af40ea7fec0474c18b3ecb46ff1e4eaa0d3f9294
mitaka <2996012639@qq.com>
2025-06-23 18:02:16 +0800

    更新耄耋头像
    卡几看打 阔阔罗 胡路诶路 马呐咋西
    
    写卡一爹 波哭哇 嘻多里 波七打打
    
    七路阔 多西卡 西拉呐一 哈路哇 马一多 西次咩打哭 阿西拉无
    
    哭拉噶里诺 呐卡一 波我次 阔我你
    
    它打打打 阔多巴 我卡 奇呐古爹
    
    奇打一丝 路打给 木呐西一 多哇卡 爹一爹莫
    
    丝哭一 我莫多 咩次足 给打
    
    写次呐 哭爹一多 哦西一
    
    一马呐拉 巴哇卡路 奇噶丝路
    
    西阿哇 写爹哭路 哦西一
    
    阿诺嘻呐 给呐卡打 波哭我
    
    嘻卡里哇 呀撒西 哭次列 打次哟
    
    哭莫马 我努爹 奇拉里 奇拉里
    
    阔阔罗 米打西爹哇 阿胡列
    
    一次西卡 火火我 奇拉里 奇拉里
    
    阿次哭 阿次哭 努拉西爹 雨哭
    
    奇米诺爹哇 多我西爹 阔嗯呐你莫 阿打打 卡一诺
    
    捏 哦捏该 多我卡 阔诺马马 哈呐撒 呐一爹一爹
    
    诶嗯我木丝 嗯爹哇 火多奇 火多卡列
    
    打列西莫噶 所列我 哟罗阔比 卡呐西 米呐噶拉
    
    阿一我 卡左诶 爹雨哭 阔多我我 打西卡咩路 哟哦你
    
    无列西哭爹 撒比西哭爹
    
    一马 打卡拉 哇卡路 奇噶西打
    
    打一写次爹 阔哇哭爹
    
    阿诺嘻呐 给呐卡打 波哭我
    
    嘻卡里哇 呀撒西 哭打奇 西咩打
    
    爹拉撒列打 写卡一
    
    撒奇火阔路 打一写 次呐嘻多
    
    阿打打 卡撒我 西打
    
    哈路哇 波哭诺 打咩奇米 诺打咩诺 呐米打 我呐噶 丝哟
    
    阿难爹 马不西 音打罗
    
    阿难爹 无次哭西 音打罗
    
    哭莫马 我努爹 奇拉里 奇拉里
    
    阔阔罗 米打西爹哇 阿胡列
    
    一次西卡 火火我 奇拉里 奇拉里
    
    阿次哭 阿次哭 努拉西爹 雨哭
    
    奇米诺爹哇 多我西爹 阔嗯呐你莫 阿打打 卡一诺
    
    捏 哦捏该 多哦卡 阔诺马马 哈呐撒 呐一爹一爹
    
    足多 足多 哈呐撒 呐一爹一爹

--- miniprogram/page/home/index.js
+++ miniprogram/page/home/index.js
@@ -11,7 +11,7 @@ Page({
     typeIndex: 1,  // 默认“生成视频”
     generationType: 'video',  // 可选值：'image' | 'video'
     avatars: [
-      { name: '头像1', src: '../../image/maodie1.jpg' },
+      { name: '头像1', src: '../../image/maodie1.png' },
       { name: '头像2', src: '../../image/maodie2.png' },
       { name: '头像3', src: '../../image/maodie3.png' },
       { name: '头像4', src: '../../image/maodie4.png' },
